3b951ed85e10a2f078a06bb56ce633050d3ee355
Me-i2004 <rapeephan_ch@cmu.ac.th>
2026-09-12 17:06:22 +0700

    lab14

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="ชื่อ-สกุล" studentId="รหัสนักศึกษา" />
+          <Footer year="2026" fullName="Rapeephan Chamcharoen" studentId="670612199" />
         </div>
       </div>
     </div>
